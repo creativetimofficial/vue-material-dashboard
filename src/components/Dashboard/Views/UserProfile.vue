1ea6f0f359c0469cca7c1d63e211b8ada28fcfc1
Dragos <dragosct10@gmail.com>
2018-04-05 17:22:14 +0300

    working in progress

--- src/components/Dashboard/Views/UserProfile.vue
+++ src/components/Dashboard/Views/UserProfile.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="">
+  <div class="md-layout">
+    <div class="md-layout-item md-size-66">
+      <edit-profile-form :data-background-color="'purple'">
 
+      </edit-profile-form>
+    </div>
+    <div class="md-layout-item md-size-33">
+      <user-card>
+
+      </user-card>
+    </div>
   </div>
 </template>
 
 <script>
+import EditProfileForm from './UserProfile/EditProfileForm.vue'
+import UserCard from './UserProfile/UserCard.vue'
+
 export default{
+  components: {
+    'edit-profile-form': EditProfileForm,
+    'user-card': UserCard
+  }
 }
 </script>
--- src/components/Dashboard/Views/UserProfile/UserCard.vue
+++ src/components/Dashboard/Views/UserProfile/UserCard.vue
@@ -1,72 +1,34 @@
 <template>
-  <div class="card card-user">
-    <div class="image">
-      <img src="static/img/background.jpg" alt="...">
+  <md-card class="md-card-profile">
+    <div class="md-card-avatar">
+       <img class="img" :src="cardUserImage">
     </div>
-    <div class="content">
-      <div class="author">
-        <img class="avatar border-white" src="static/img/faces/face-2.jpg" alt="...">
-        <h4 class="title">Chet Faker
-          <br>
-          <a href="#">
-            <small>@chetfaker</small>
-          </a>
-        </h4>
-      </div>
-      <p class="description text-center">
-        "I like the way you work it
-        <br> No diggity
-        <br> I wanna bag it up"
+
+    <md-card-content>
+      <h6 class="category text-gray">CEO / Co-Founder</h6>
+      <h4 class="card-title">Alec Thompson</h4>
+      <p class="card-content">
+          Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...
       </p>
-    </div>
-    <hr>
-    <div class="text-center">
-      <div class="row">
-        <div v-for="(info,index) in details" :class="getClasses(index)">
-          <h5>{{info.title}}
-            <br>
-            <small>{{info.subTitle}}</small>
-          </h5>
-        </div>
-      </div>
-    </div>
-  </div>
+      <md-button class="md-raised md-primary md-round">Follow</md-button>
+    </md-card-content>
+  </md-card>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        details: [
-          {
-            title: '12',
-            subTitle: 'Files'
-          },
-          {
-            title: '2GB',
-            subTitle: 'Used'
-          },
-          {
-            title: '24,6$',
-            subTitle: 'Spent'
-          }
-        ]
-      }
-    },
-    methods: {
-      getClasses (index) {
-        var remainder = index % 3
-        if (remainder === 0) {
-          return 'col-md-3 col-md-offset-1'
-        } else if (remainder === 2) {
-          return 'col-md-4'
-        } else {
-          return 'col-md-3'
-        }
-      }
+export default {
+  props: {
+    cardUserImage: {
+      type: String,
+      default: require('@/assets/img/faces/marc.jpg')
+    }
+  },
+  data () {
+    return {
     }
   }
+}
 
 </script>
 <style>
-  
+
 </style>
