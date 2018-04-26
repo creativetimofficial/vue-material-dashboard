d20457f846a319f4d2450ff3bf1246ecfed40290
Dragos <dragosct10@gmail.com>
2018-04-26 12:33:06 +0300

    added metatags + pixel facebook

--- src/components/Dashboard/Layout/DashboardLayout.vue
+++ src/components/Dashboard/Layout/DashboardLayout.vue
@@ -40,6 +40,6 @@ export default {
       sidebarBackground: 'green',
       sidebarBackgroundImage: require('@/assets/img/sidebar-1.jpg')
     }
-  },
+  }
 }
 </script>
--- src/components/Dashboard/Layout/Extra/FixedPlugin.vue
+++ src/components/Dashboard/Layout/Extra/FixedPlugin.vue
@@ -60,63 +60,63 @@
   </div>
 </template>
 <script>
-  import Vue from 'vue'
-  import SocialSharing from 'vue-social-sharing'
-  import VueGitHubButtons from 'vue-github-buttons'
-  import 'vue-github-buttons/dist/vue-github-buttons.css'
+import Vue from 'vue'
+import SocialSharing from 'vue-social-sharing'
+import VueGitHubButtons from 'vue-github-buttons'
+import 'vue-github-buttons/dist/vue-github-buttons.css'
 
-  Vue.use(SocialSharing)
-  Vue.use(VueGitHubButtons, {useCache: true})
-  export default {
-    data () {
-      return {
-        documentationLink: 'http://vuejs.creative-tim.com/vue-now-ui-dashboard-pro/documentation/#/getting-started',
-        shareUrl: 'https://www.creative-tim.com/product/vue-now-ui-dashboard-pro',
-        buyUrl: 'https://www.creative-tim.com/product/vue-now-ui-dashboard-pro',
-        freeUrl: 'https://www.creative-tim.com/product/vue-now-ui-dashboard',
-        isOpen: false,
-        sidebarColors: [
-          {color: 'purple', active: false},
-          {color: 'blue', active: false},
-          {color: 'green', active: true},
-          {color: 'orange', active: false},
-          {color: 'red', active: false}
-        ],
-        sidebarImages: [
-          {image: require('@/assets/img/sidebar-1.jpg'), active: true},
-          {image: require('@/assets/img/sidebar-2.jpg'), active: false},
-          {image: require('@/assets/img/sidebar-3.jpg'), active: false},
-          {image: require('@/assets/img/sidebar-4.jpg'), active: false}
-        ]
-      }
+Vue.use(SocialSharing)
+Vue.use(VueGitHubButtons, {useCache: true})
+export default {
+  data () {
+    return {
+      documentationLink: 'http://vuejs.creative-tim.com/vue-now-ui-dashboard-pro/documentation/#/getting-started',
+      shareUrl: 'https://www.creative-tim.com/product/vue-now-ui-dashboard-pro',
+      buyUrl: 'https://www.creative-tim.com/product/vue-now-ui-dashboard-pro',
+      freeUrl: 'https://www.creative-tim.com/product/vue-now-ui-dashboard',
+      isOpen: false,
+      sidebarColors: [
+        {color: 'purple', active: false},
+        {color: 'blue', active: false},
+        {color: 'green', active: true},
+        {color: 'orange', active: false},
+        {color: 'red', active: false}
+      ],
+      sidebarImages: [
+        {image: require('@/assets/img/sidebar-1.jpg'), active: true},
+        {image: require('@/assets/img/sidebar-2.jpg'), active: false},
+        {image: require('@/assets/img/sidebar-3.jpg'), active: false},
+        {image: require('@/assets/img/sidebar-4.jpg'), active: false}
+      ]
+    }
+  },
+  methods: {
+    toggleDropDown () {
+      this.isOpen = !this.isOpen
     },
-    methods: {
-      toggleDropDown () {
-        this.isOpen = !this.isOpen
-      },
-      closeDropDown () {
-        this.isOpen = false
-      },
-      toggleList (list, itemToActivate) {
-        list.forEach((listItem) => {
-          listItem.active = false
-        })
-        itemToActivate.active = true
-      },
-      updateValue (name, val) {
-        console.log(name);
-        this.$emit(`update:${name}`, val)
-      },
-      changeSidebarBackground (item) {
-        this.$emit('update:color', item.color)
-        this.toggleList(this.sidebarColors, item)
-      },
-      changeSidebarImage (item) {
-        this.$emit('update:image', item.image)
-        this.toggleList(this.sidebarImages, item)
-      }
+    closeDropDown () {
+      this.isOpen = false
+    },
+    toggleList (list, itemToActivate) {
+      list.forEach((listItem) => {
+        listItem.active = false
+      })
+      itemToActivate.active = true
+    },
+    updateValue (name, val) {
+      console.log(name)
+      this.$emit(`update:${name}`, val)
+    },
+    changeSidebarBackground (item) {
+      this.$emit('update:color', item.color)
+      this.toggleList(this.sidebarColors, item)
+    },
+    changeSidebarImage (item) {
+      this.$emit('update:image', item.image)
+      this.toggleList(this.sidebarImages, item)
     }
   }
+}
 </script>
 <style>
   .centered-row {
