f08f528d37c877c43d39c0b8451471600948880e
Dragos <dragosct10@gmail.com>
2018-05-01 18:35:40 +0300

    changes in index.js components

--- src/components/index.js
+++ src/components/index.js
@@ -8,15 +8,11 @@ import NavTabsTable from './Tables/NavTabsTable.vue'
 import OrderedTable from './Tables/OrderedTable.vue'
 import SimpleTable from './Tables/SimpleTable.vue'
 
-// DropDown
-import Dropdown from './Dropdown.vue'
-
 export {
   ChartCard,
   NavTabsCard,
   StatsCard,
   NavTabsTable,
   OrderedTable,
-  SimpleTable,
-  Dropdown
+  SimpleTable
 }
