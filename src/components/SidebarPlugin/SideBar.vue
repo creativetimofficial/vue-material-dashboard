dccb305130bb2df6b162bda3e8a0deb86bb6b9e7
Dragos <dragosct10@gmail.com>
2018-05-03 16:35:56 +0300

    changes

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -42,7 +42,7 @@ export default{
     },
     backgroundImage: {
       type: String,
-      default: require('@/assets/img/sidebar-1.jpg')
+      default: require('@/assets/img/sidebar-2.jpg')
     },
     imgLogo: {
       type: String,
