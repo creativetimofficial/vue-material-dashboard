9196bc90469860f06ccf4986757fec84960f501a
zhangchao <544262408@qq.com>
2018-08-31 11:37:53 +0800

    页脚

--- src/pages/Layout/ContentFooter.vue
+++ src/pages/Layout/ContentFooter.vue
@@ -2,29 +2,32 @@
   <footer class="footer">
     <div class="container">
         <nav>
-            <!-- <ul>
-              <li>
-                <router-link :to="{path:'/dashboard'}">Home</router-link>
-              </li>
+            <ul>
+
                 <li>
-                    <a href="#">
-                        Company
+                    <a href="https://www.lambda.im/">
+                        Home
                     </a>
                 </li>
                 <li>
-                    <a href="#">
-                        Portfolio
+                    <a href="https://twitter.com/Lambdaim">
+                        Twitter
                     </a>
                 </li>
                 <li>
-                    <a href="#">
-                        Blog
+                    <a href="https://www.linkedin.com/company/lambdaim/">
+                        Linkedin
                     </a>
                 </li>
-            </ul> -->
+                <li>
+                    <a href="https://mp.weixin.qq.com/s/rW3pOxzJwI1HXrTESTYxWA">
+                        WeChart
+                    </a>
+                </li>
+            </ul>
         </nav>
         <div class="copyright text-center">
-          &copy; {{ new Date().getFullYear() }} <a href="https://www.lambda.im/" target="_blank">lambda</a>
+          &copy; {{ new Date().getFullYear() }} <a href="https://www.lambda.im/" target="_blank">Lambda</a>  Providing Unlimited Storage Capabilities
         </div>
     </div>
   </footer>
