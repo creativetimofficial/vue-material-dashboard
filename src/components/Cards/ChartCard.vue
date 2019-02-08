cd5b13a35d125302fb25e06a51b8582cd9c3caeb
Dragos <dragosct10@gmail.com>
2019-02-08 16:19:36 +0200

    chart card component fixed

--- src/components/Cards/ChartCard.vue
+++ src/components/Cards/ChartCard.vue
@@ -1,7 +1,7 @@
 <template>
   <md-card>
     <md-card-header class="card-chart" :data-background-color="dataBackgroundColor">
-      <vue-chartist :data="chartData" :options="chartOptions" :type="chartType"></vue-chartist>
+      <div :id="chartId" class="ct-chart"></div>
     </md-card-header>
 
     <md-card-content>
@@ -14,14 +14,17 @@
   </md-card>
 </template>
 <script>
-import VueChartist from "v-chartist";
-
 export default {
-  components: {
-    "vue-chartist": VueChartist
-  },
   name: "chart-card",
   props: {
+    footerText: {
+      type: String,
+      default: ""
+    },
+    headerTitle: {
+      type: String,
+      default: "Chart title"
+    },
     chartType: {
       type: String,
       default: "Line" // Line | Pie | Bar
@@ -30,6 +33,12 @@ export default {
       type: Object,
       default: () => {
         return {};
+      }
+    },
+    chartResponsiveOptions: {
+      type: Array,
+      default: () => {
+        return [];
       }
     },
     chartData: {
@@ -45,6 +54,39 @@ export default {
       type: String,
       default: ""
     }
+  },
+  data() {
+    return {
+      chartId: "no-id"
+    };
+  },
+  methods: {
+    /***
+     * Initializes the chart by merging the chart options sent via props and the default chart options
+     */
+    initChart() {
+      var chartIdQuery = `#${this.chartId}`;
+      this.$Chartist[this.chartType](
+        chartIdQuery,
+        this.chartData,
+        this.chartOptions
+      );
+    },
+    /***
+     * Assigns a random id to the chart
+     */
+    updateChartId() {
+      var currentTime = new Date().getTime().toString();
+      var randomInt = this.getRandomInt(0, currentTime);
+      this.chartId = `div_${randomInt}`;
+    },
+    getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  },
+  mounted() {
+    this.updateChartId();
+    this.$nextTick(this.initChart);
   }
 };
 </script>
