d07e021bdb096494c96af9333501fd9c671ebc62
Dragos <dragosct10@gmail.com>
2020-03-12 17:48:34 +0200

    refactor(config): update version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
   themeConfig: {
     repo: 'creativetimofficial/vue-material-dashboard',
     nav: [
-      { text: 'v1.3.2', link: '/' }
+      { text: 'v1.4.0', link: '/' }
     ],
     sidebar: [
       {
