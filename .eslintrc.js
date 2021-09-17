6e4f3ffecabab8a729607fb2aa8dec36abe2507c
Patrick <patrick@portaltelemedicina.com.br>
2021-09-17 19:42:19 -0300

    fixed lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,7 @@ module.exports = {
     node: true
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
+  rules: {},
   parserOptions: {
     parser: "babel-eslint"
   }
