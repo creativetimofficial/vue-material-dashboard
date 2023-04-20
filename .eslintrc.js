d463af46851908dd35b10e947a618d4a13229e25
Maisam Afshar <maisam.afshar@gmail.com>
2023-04-20 10:04:56 +0430

    vue/multi-word-component-names: off

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,6 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-  },
-  parserOptions: {
-    parser: "babel-eslint",
+    "vue/multi-word-component-names": "off",
   },
 };
